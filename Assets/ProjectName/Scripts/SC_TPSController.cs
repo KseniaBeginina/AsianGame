efffadb491a557b4d8be73a7e827f146efe3d8b9
LilStpd <4646rc.if@gmail.com>
2024-03-18 22:51:51 +0400

    Walking animations
    Changed player object, added walking animations

--- Assets/ProjectName/Scripts/SC_TPSController.cs
+++ Assets/ProjectName/Scripts/SC_TPSController.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class SC_TPSController : MonoBehaviour
 {
-    public float speed = 7.5f;
-    public float jumpSpeed = 8.0f;
+    public float speed = 4f;
+    // public float rotationSpeed = 0.5f;
+    // public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    
+    //Camera
     public Transform playerCameraParent;
     public float lookSpeed = 1f;
     public float lookXLimit = 60.0f;
 
     CharacterController characterController;
+    private Animator animator;
     Vector3 moveDirection = Vector3.zero;
     Vector2 rotation = Vector2.zero;
 
@@ -20,6 +24,7 @@ public class SC_TPSController : MonoBehaviour
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         rotation.y = transform.eulerAngles.y;
     }
@@ -28,22 +33,24 @@ public class SC_TPSController : MonoBehaviour
     {
         if (characterController.isGrounded)
         {
-            // We are grounded, so recalculate move direction based on axes
+            
             Vector3 forward = transform.TransformDirection(Vector3.forward);
             Vector3 right = transform.TransformDirection(Vector3.right);
             float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
             float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
             moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+            // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed);
 
-            if (Input.GetButton("Jump") && canMove)
-            {
-                moveDirection.y = jumpSpeed;
-            }
+            // Установка параметра "Speed" в аниматоре
+            float moveSpeed = Mathf.Clamp(moveDirection.magnitude / speed, 0f, 4f);
+            animator.SetFloat("speed", moveSpeed);
+
+            // if (Input.GetButton("Jump") && canMove)
+            // {
+            //     moveDirection.y = jumpSpeed;
+            // }
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
         moveDirection.y -= gravity * Time.deltaTime;
 
         // Move the controller
